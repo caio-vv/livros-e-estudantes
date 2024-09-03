2ffe91d6b80f64ff7620a658e1f33b5673f751c3
Caio <caioviniciusleitelisboa@gmail.com>
2024-09-03 08:38:10 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const estudante_router = require("./routes/estudante.js")
+const aluguel_router = require("./routes/aluguel.js")
+const livro_router = require("./routes/livro.js")
+const app = express()
+const port = 5000
+
+
+app.use(express.json())
+
+app.use("/estudante", estudante_router)
+app.use("/aluguel", aluguel_router)
+app.use("/livro", livro_router)
+
+
+app.listen(port, () => {
+    console.log(`Server running in ${port} port`)
+})
